09510e7e50e72eedf7b8663221a4b03c077d7284
Anirudh Sylendranath <ani.22691@gmail.com>
2021-03-20 22:24:36 +0530

    Adding docker-compose support

---
+++ docker-entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+python manage.py migrate --noinput
+
+if [ -n "$DJANGO_SUPERUSER_USERNAME" ] && [ -n "$DJANGO_SUPERUSER_PASSWORD" ] ; then
+    python manage.py createsuperuser --no-input
+fi
+
+exec 2>&1 \
+    python manage.py runserver
